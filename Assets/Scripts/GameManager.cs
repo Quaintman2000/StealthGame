01b1986c807e8102f36c9015818388080ed6eb2e
Alec <Quaintman2000@gmail.com>
2020-02-21 20:23:03 -0700

    Added Sprites, camera follows player, and some UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Transform playerTransform;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        //grabs the transforms of both the main camera and player
+        cameraTransform = GameObject.Find("Main Camera").GetComponent<Transform>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes the camera move along with the player
+        cameraTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, 
+            cameraTransform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform tf;
+    public float speed;
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position += -tf.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed);
+        }
+    }
+}
